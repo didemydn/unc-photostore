d44860592c080a433b5865bf1f491acd4a8cd84d
Didem Cakir <didemydn@gmail.com>
2024-09-08 18:53:06 +0200

    Home-dashboard-login and contact route created

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Dashboard from './pages/Dashboard/Dashboard';
+import Contact from './pages/Contact/Contact';
+import Login from './pages/Login/Login';
+
+function App() {
+  return (
+    <div className="App">      
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home></Home>}/>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}/>
+        <Route path="/contact" element={<Contact></Contact>}/>
+        <Route path="/login" element={<Login></Login>}/>
+      </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
